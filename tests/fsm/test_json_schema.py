db391f20c4afb97bf59d89e1cf2f6b509c582a70
Victoria Terenina (torymur) <torymur@gmail.com>
2025-01-16 20:15:00 +0000

    Test python bindings interfaces for json schema

--- tests/fsm/test_json_schema.py
+++ tests/fsm/test_json_schema.py
@@ -1,65 +1,53 @@
 import json
 import re
-from typing import Literal, Union
 
-import pytest
-from outlines_core.fsm.json_schema import build_regex_from_schema, to_regex
-from pydantic import BaseModel, Field
+from outlines_core.fsm.json_schema import (  # noqa: F401
+    BOOLEAN,
+    DATE,
+    DATE_TIME,
+    EMAIL,
+    INTEGER,
+    NULL,
+    NUMBER,
+    STRING,
+    STRING_INNER,
+    TIME,
+    URI,
+    UUID,
+    WHITESPACE,
+    build_regex_from_schema,
+)
+from pydantic import BaseModel
 
 
-@pytest.mark.parametrize("whitespace_pattern", [None, r"[\n ]*", "abc"])
-def test_json_schema_custom_whitespace_pattern(whitespace_pattern):
-    """assert whitespace_pattern setting respected"""
-
-    class MockModel(BaseModel):
+def test_build_regex_from_json_schema():
+    class FooBar(BaseModel):
         foo: int
         bar: str
 
-    schema = json.dumps(MockModel.model_json_schema())
+    schema = json.dumps(FooBar.model_json_schema())
 
-    # assert any ws pattern can be used
-    if whitespace_pattern == "abc":
-        build_regex_from_schema(schema, whitespace_pattern)
-        return
+    regex = build_regex_from_schema(schema)
+    expected = """{"foo" : 4 ,"bar":"baz    baz baz bar"}"""
+    assert re.fullmatch(regex, expected)
 
-    pattern = build_regex_from_schema(schema, whitespace_pattern)
-
-    mock_result_mult_ws = (
-        """{     "foo"   :   4, \n\n\n   "bar": "baz    baz baz bar"\n\n}"""
-    )
-    mock_result_maybe_ws = """{"foo" : 4 ,"bar":"baz    baz baz bar"}"""
-
-    match_default_ws = re.fullmatch(pattern, mock_result_maybe_ws)
-    if whitespace_pattern is None:
-        assert match_default_ws
-    else:
-        assert re.fullmatch(pattern, mock_result_mult_ws)
+    # any whitespace pattern can be used
+    regex = build_regex_from_schema(schema, r"[\n ]*")
+    expected = """{     "foo"   :   4, \n\n\n   "bar": "baz    baz baz bar"\n\n}"""
+    assert re.fullmatch(regex, expected)
 
 
-def test_one_of_doesnt_produce_illegal_lookaround():
-    """Reproduces failure in https://github.com/dottxt-ai/outlines/issues/823"""
-
-    class Cat(BaseModel):
-        pet_type: Literal["cat"]
-        meows: int
-
-    class Dog(BaseModel):
-        pet_type: Literal["dog"]
-        barks: float
-
-    class Model(BaseModel):
-        pet: Union[Cat, Dog] = Field(..., discriminator="pet_type")
-        n: int
-
-    json_schema = json.dumps(Model.model_json_schema())
-    build_regex_from_schema(json_schema, whitespace_pattern=None)
-
-
-def test_match_object():
-    test_regex = to_regex(
-        {
-            "type": "object",
-            "maxProperties": 0,
-        }
-    )
-    assert test_regex == r"\{[ ]?\}"
+def test_types_presence():
+    assert BOOLEAN
+    assert DATE
+    assert DATE_TIME
+    assert EMAIL
+    assert INTEGER
+    assert NULL
+    assert NUMBER
+    assert STRING
+    assert STRING_INNER
+    assert TIME
+    assert URI
+    assert UUID
+    assert WHITESPACE
