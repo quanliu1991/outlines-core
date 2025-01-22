f08d62aac1dc62a65526e41c4c0476eed73360c9
Yvan Sraka <yvan@sraka.xyz>
2025-01-22 11:24:57 +0100

    Add nix configs, and uv lock files

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { flake-utils, nixpkgs, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShell = import ./shell.nix { inherit pkgs system; };
+      });
+}
